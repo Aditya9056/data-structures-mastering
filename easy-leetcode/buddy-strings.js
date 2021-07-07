c38f84c2b86f40a53e869bf50b5c8f00b9a90c66
Aditya Jain <vvasujain@gmail.com>
2021-07-07 23:51:39 +0530

    buddy-strings

---
+++ easy-leetcode/buddy-strings.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+
+ Array.prototype.swap = function (x,y) {
+    var b = this[x];
+    this[x] = this[y];
+    this[y] = b;
+    return this.join('');
+  }
+
+ var buddyStrings = function(s, goal) {
+
+    let swapHappen = false
+    
+    for( i=0; i<s.length; i++ ){
+
+        if( s[i] !== goal[i] ){
+
+            for( j=i+1; j<s.length; j++ ){
+                
+                // check if one swap can make strings equal
+                if( goal[i] === s[j] ){
+
+                    // console.log("changing", s)
+                    s = s.split('');
+                    s = s.swap(j, i)
+
+                    // console.log("changing", s)
+
+                    return (s === goal) ? true : false
+                }
+
+            }    
+        }   
+    }
+
+    return (s === goal) && swapHappen ? true : false
+    
+};
+
+let s = "ab", goal = "ab"
+
+console.log(buddyStrings(s,goal))
