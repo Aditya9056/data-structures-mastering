153d3d92f244535e88b0b8a8de48767f96bb7c49
Aditya Jain <vvasujain@gmail.com>
2021-07-12 03:40:53 +0530

    done remove-element

--- easy-leetcode/remove-element.js
+++ easy-leetcode/remove-element.js
@@ -1,8 +1,21 @@
-/**
- * @param {number[]} nums
- * @param {number} val
- * @return {number}
- */
- var removeElement = function(nums, val) {
+var removeElement = function(nums, val) {
+    let low = 0
     
-};
+    for(i=0; i<nums.length; i++){
+        
+      if( nums[i] !== val ){
+          nums[low++] = nums[i]
+      }
+    }
+
+    return low
+}
+
+let nums = [3,2,2,3]
+let val = 3
+let expectedNums = [2, 2]; 
+
+
+const k = removeElement(nums, val)
+
+console.log(k.length, k)
