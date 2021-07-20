5d951fd68beb50732744a35de7aea1f5c867972a
Aditya Jain <vvasujain@gmail.com>
2021-07-20 23:10:44 +0530

    merge sorted linked list

--- easy-leetcode/merge-sorted-linkedlist.js
+++ easy-leetcode/merge-sorted-linkedlist.js
@@ -5,45 +5,34 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
 
-class Node{
-    constructor(data, next = null){
-        this.data = data
-        this.next = next
-    }
-}
-
-class LinkedList{
-    constructor(){
-        this.head = null
-    }
-}
-
-const insertAt = (list, ele) => {
-
-    for( i=0;i<list.length; i++ ){
-        
-        if( ele < list[i] ){
-            
-        }
-    }
-
-}
 
  var mergeTwoLists = function(l1, l2) {
-
-    l1.forEach(element => {
-        console.log(element)
-
-    });
     
+    let l3 = new ListNode()
+    let head = l3
+    
+    while( l1 !== null && l2 !== null ){
+        
+        if( l1.val <= l2.val ){
+            
+            l3.next = new ListNode(l1.val)
+            l1 = l1.next
+            
+        } else {
+            
+            l3.next = new ListNode(l2.val)
+            l2 = l2.next
+        }
+        
+        l3 = l3.next
+    }
+
+    if( l1 === null )
+        l3.next = l2
+
+    if( l2 === null )
+        l3.next = l1
+    
+    return head.next
 };
-
-l1 = [1,2,4], l2 = [1,3,4]
-
-mergeTwoLists(l1, l2)
