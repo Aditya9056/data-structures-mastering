4f6c4ac7d8eaa35b382eb6167116b7f7ab8dac39
Aditya Jain <vvasujain@gmail.com>
2021-07-25 23:13:37 +0530

    maximum-subarray with Kadane's Algorithm

--- easy-leetcode/maximum-subarray.js
+++ easy-leetcode/maximum-subarray.js
@@ -2,6 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
  var maxSubArray = function(nums) {
-    
+    let sum = 0, max = nums[0]
+
+for(let i = 0; i < nums.length; i++){
+
+    if( sum < 0 ) sum = 0
+
+    if( sum >= 0 ) sum += nums[i]
+
+    if( sum > max ) max = sum
+}
+
+return max
 };
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
